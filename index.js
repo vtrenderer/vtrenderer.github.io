1ff31efd55d3a2034f77d1b4abad3e826a42118f
james <ojameskola03@gmail.com>
2024-04-14 22:51:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,10 +8,8 @@ var firebaseConfig = {
   appId: "1:583252692015:web:9615861360f2bcc69a8ada",
   measurementId: "G-KR9Z6EHF56"
 };
-document.querySelector("#full_name").innerHTML = firebaseConfig[apiKey];
 
 firebase.initializeApp(firebaseConfig);
-document.querySelector("#email").innerHTML = username;
 var database = firebase.database()
   
 var user_ref = database.ref(document.getElementById('username').value)
@@ -24,3 +22,9 @@ user_ref.on('value', function(snapshot) {
   var address = userData.address
   var date_of_birth = userData.date_of_birth
 })
+
+document.querySelector("#full_name").innerHTML = full_name;
+document.querySelector("#email").innerHTML = email;
+document.querySelector("#course").innerHTML = course;
+document.querySelector("#address").innerHTML = address;
+document.querySelector("#date_of_birth").innerHTML = date_of_birth;
